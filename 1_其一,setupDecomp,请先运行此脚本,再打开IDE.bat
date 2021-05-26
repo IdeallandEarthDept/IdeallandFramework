a791fa2d1cbef2b65ba83d38786da1148ffd5829
ZekerZhayard <trees1000@qq.com>
2021-05-26 21:42:43 +0800

    Upgrade Gradle version. Use mirror urls as much as possible.

---
+++ 1_其一,setupDecomp,请先运行此脚本,再打开IDE.bat
@@ -0,0 +1,9 @@
+@echo off
+echo %JAVA_TOOL_OPTIONS% | find "-Dfile.encoding=" > nul
+if errorlevel 1 (
+    setx JAVA_TOOL_OPTIONS "%JAVA_TOOL_OPTIONS% -Dfile.encoding=UTF-8"
+    set "JAVA_TOOL_OPTIONS=%JAVA_TOOL_OPTIONS% -Dfile.encoding=UTF-8"
+)
+chcp 65001
+call gradlew.bat setupDecompWorkspace
+pause
